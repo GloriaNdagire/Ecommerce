57173f5a3de40ecf11fac3ffd404d9249919295e
GloriaNdagire <gloriandagire02@gmail.com>
2025-07-14 22:09:01 +0300

    changed the logo, and added some animations and transitions

--- my-ecommerce/src/app/Assign/page.jsx
+++ my-ecommerce/src/app/Assign/page.jsx
@@ -27,6 +27,9 @@ const assign =() => {
               "/img/img1.jpg",
               "/img/img1.jpg",
               "/img/img1.jpg",
+              "/img/img4.jpg",
+              "/img/img1.jpg",
+              "/img/img2.jpg",
             ],
             shoes: [
               "/img/img2.jpg",
@@ -37,16 +40,20 @@ const assign =() => {
               "/img/img2.jpg",
             ],
             accessories: [
-              "/img/img10.jpg",
-              "/img/img10.jpg",
-              "/img/img10.jpg",
-              "/img/img10.jpg",
-              "/img/img10.jpg",
-              "/img/img10.jpg",
-              "/img/img10.jpg",
+              "/img/img4.jpg",
+              "/img/img4.jpg",
+              "/img/img4.jpg",
+              "/img/img4.jpg",
+              "/img/img4.jpg",
+              "/img/img4.jpg",
+         
             ]
            };
-
+           {/*
+           if setCategory() === (("Phones") => {
+                   
+})
+ */}
      return (
         
             <div>
@@ -54,16 +61,16 @@ const assign =() => {
                 <div className="flex justify-between bg-gray-100 fixed top-0 left-0 z-50 shadow-md w-full">
                   
                   <div >
-                    <img src="./img/logo.png" className="w-1/9"></img>
+                    <img src="./img/logo4.png" className="w-1/4 animate-bounce "></img>
                   </div>
-                  <div className="sm:justify-center md:justify-end flex">
-                    <a href="/" className="text-semibold text-xl text-pink-900 bg-gray-300 hover:bg-gray-500 rounded m-3 mb-8 ">Log In</a>
+                  <div className="text-white-300 items-center sm:justify-center md:justify-end flex bg-gray-300 rounded m-9 pl-8 pr-8 mr-10 transition duration-300 ease-in-out hover:bg-gray-800  transform hover:scale-110">
+                    <a href="" className="text-semibold text-xl ">LOG IN</a>
                   </div>
                 </div>
                  
 
         <div className="flex">
-            <div className=" bg-gray-300 w-1/5 pb-25 shadow-md rounded-10 mt-20">
+            <div className=" bg-gray-300 w-1/5 pb-25 shadow-md rounded-10 mt-34">
               <span className="flex flex-col m-1 mb-1">
                 <a href="" className="mt-20 border-b border-gray-100" id="phones" onClick={(e) => handleCategoryClick(e, "Phones")}>Phones</a>
                 <a href="" className="mt-35 border-b border-gray-100" id="fashion" onClick={(e) => handleCategoryClick(e, "fashion")}>Fashion</a>
@@ -76,9 +83,9 @@ const assign =() => {
             
 
             {/* Render images dynamically based on selected category */}
-            <div className="flex flex-wrap ml-20 mt-15">
+            <div className="flex flex-wrap ml-18 mt-25">
               {data[category] && data[category].map((imgSrc, idx) => (
-                <div key={idx} className="bg-gray-300 shadow-md w-[290px] h-[300px] m-10 p-3">
+                <div key={idx} className="bg-gray-300 shadow-md w-[290px] h-[300px] m-10 p-3 rounded transition ease-in-out duration-200 transform hover:scale-110">
                   <img src={imgSrc} alt={category + idx} className="w-full h-full object-cover" />
                 </div>
               ))}
@@ -105,10 +112,6 @@ const assign =() => {
                 </footer>
              </div>
             </div>
-
-
-        
-
-     )
+            )
 }
  export default assign;
